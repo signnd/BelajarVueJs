7a3523d5633cb99ffe040ff0f5c1fb82132500bb
Pryandika <91819749+Pryandika@users.noreply.github.com>
2021-12-20 16:39:10 +0800

    Adding Basic Navbar
    First commit, adding navbar

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+<!-- Navbar atas -->
+  <div id="nav">
+  <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Rumah sakit apa</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="navbar-nav mr-auto">
+            <router-link to="/" class="nav-item nav-link">Home</router-link>
+            <router-link to="/About" class="nav-item nav-link">About</router-link>
+            <router-link to="/Career" class="nav-item nav-link">Career</router-link>
+            <router-link to="/Login" class="nav-item nav-link">Login</router-link>
+        </div>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+ </div>
+  <router-view/>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 10px;
+  }
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  
+}
+
+#nav a.router-link-exact-active {
+  color: #6ec3ff;
+}
+
+#nav form{
+    color: #6ec3ff;
+}
+</style>
