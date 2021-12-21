106b140b9a47086071dff5a56b7db055c1500c16
Pryandika <91819749+Pryandika@users.noreply.github.com>
2021-12-21 14:11:43 +0800

    costumize navbar
    menambahkan hover

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
 <!-- Navbar atas -->
-  <div id="nav">
+<div class="header-area" id="nav">
   <nav class="container navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Rumah sakit apa</a>
+    <a class="navbar-brand" href="#">Rumah sakit</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <div class="navbar-nav mr-auto">
-            <router-link to="/" class="nav-item nav-link">Home</router-link>
-            <router-link to="/About" class="nav-item nav-link">About</router-link>
-            <router-link to="/Career" class="nav-item nav-link">Career</router-link>
-            <router-link to="/Login" class="nav-item nav-link">Login</router-link>
-        </div>
-        <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+      <div class="navbar-nav mr-auto">
+        <router-link to="/" class="nav-item nav-link">Home</router-link>
+        <router-link to="/About" class="nav-item nav-link">About</router-link>
+        <router-link to="/Career" class="nav-item nav-link">Career</router-link>
+        <router-link to="/Login" class="nav-item nav-link">Login</router-link>
+      </div>
     </div>
-</nav>
- </div>
+  </nav>
+</div>
+<div>
+  <h1>test</h1>
+</div>
   <router-view/>
 </template>
+
 
 
 <style>
@@ -45,6 +45,10 @@
 }
 
 #nav a.router-link-exact-active {
+  color: #6ec3ff;
+}
+
+#nav a:hover{
   color: #6ec3ff;
 }
 
