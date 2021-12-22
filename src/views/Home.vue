b627afe30a4c46c2c9447f895fde014acb596805
Pryandika <91819749+Pryandika@users.noreply.github.com>
2021-12-22 10:27:50 +0800

    Adding slideshow & text
    Menambah image slideshow dan text ke home.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,59 @@
 <template>
-  <div class="home">
-    <img :src="images.sample" class="img-fluid" alt="Responsive image">
-  </div>
-  <h1>asd</h1>
+<!-- Slideshow -->
+  <section>
+    <div id="slideshow" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#slideshow" data-slide-to="0" class="active"></li>
+        <li data-target="#slideshow" data-slide-to="1"></li>
+        <li data-target="#slideshow" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100 " :src="images.cover1" alt="First slide">
+          <div class="carousel-caption d-none d-md-block text-dark text-left align-middle">
+            <h1>Judul</h1>
+            <p>Pada suatu hari</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" :src="images.cover1" alt="Second slide">
+          <div class="carousel-caption d-none d-md-block text-dark text-left align-middle">
+            <h1>Judul</h1>
+            <p>Pada suatu hari</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" :src="images.cover1" alt="Third slide">
+          <div class="carousel-caption d-none d-md-block text-dark text-left align-middle">
+            <h1>Judul</h1>
+            <p>Pada suatu hari</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#slideshow" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#slideshow" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </section>
+
+  <!--  -->
+  <section>
+    <h1>Blm isi</h1>
+  </section>
 </template>
+    
     
 <script>
 export default {
     data() {
         return {
             images: {
-                sample: require('@/assets/hero/hero.png')
+                cover1: require('@/assets/hero/hero.png')
             }
         }
     }
@@ -18,4 +61,25 @@ export default {
 </script>
     
 <style lang="css">
+.container {
+  position: relative;
+  text-align: left;
+  color: rgb(0, 0, 0);
+}
+
+.judul1 {
+  position: absolute;
+  top: 35%;
+  left: 30%;
+  transform: translate(-50%, -50%);
+  font-size: 50px;
+}
+
+.carousel-caption{
+  top: 200px;
+}
+
+.carousel-inner .carousel-item{
+  height: 550px;
+}
 </style> 
