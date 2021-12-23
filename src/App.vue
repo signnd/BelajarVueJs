33d66bd0b954a359a7a526e72891ba717bb71b9d
signnd <nfffqdb@gmail.com>
2021-12-23 15:45:58 +0800

    auto close nav on click (not working on mine)

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,19 @@
             <img :src="images.logo" height="38" alt="logo">
           </a>
         </nav>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <div class="navbar-nav my-1">
-            <router-link to="/" class="nav-item nav-link fs-4 mx-2 nav-fill active">Home</router-link>
-            <router-link to="/Doctors" class="nav-item nav-link fs-4 mx-2">Doctors</router-link>            
-            <router-link to="/Contact" class="nav-item nav-link fs-4 mx-2">Contact</router-link>            
-            <router-link to="/About" class="nav-item nav-link fs-4 mx-2 mr-5">About</router-link>
-
+            <router-link to="/" class="nav-item nav-link fs-4 mx-2 nav-fill active" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">Home</router-link>
+            <router-link to="/Doctors" class="nav-item nav-link fs-4 mx-2" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">Doctors</router-link>            
+            <router-link to="/Contact" class="nav-item nav-link fs-4 mx-2" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">Contact</router-link>            
+            <router-link to="/About" class="nav-item nav-link fs-4 mx-2" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">About</router-link>
             <!-- Search bar -->
-            <form class="form-inline my-2 my-lg-1">
-              <input class="form-control mr-sm-2" type="search" placeholder="Search">
+            <form class="form-inline d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search">
               <button class="btn btn-outline-primary my-sm-0" type="submit">Search</button>
             </form>
           </div>
