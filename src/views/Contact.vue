027631460fd17500a6c117f3325bb32f9065c0b4
Pasek Ryandika <91819749+Pryandika@users.noreply.github.com>
2021-12-24 14:06:42 +0800

    Added Google Maps on Contact Page
    Nambahin map dan alamat di contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,11 +1,42 @@
 <template>
-<section>
-<div class="contact" id="contact">
-    <h1 class="mt-5">Hubungi kami</h1>
-    <h1 class="mt-5">TES</h1>
-    <h5 align="left" class="mt-2">Maaf kami belum bisa dihubungi</h5>
+  <div class="contact">
+    <section class="section pb-5">
+
+      <!--Section heading-->
+      <h2 class="section-heading h1 pt-4">Contact us</h2>
+      <p class="section-description pb-4">Mikirin text apa diisi</p>
+
+      <!-- Google Maps -->
+      <div class="container col-lg-7">
+        <div id="map-container-google-11" class="z-depth-1-half map-container-6" style="height: 400px">
+          <iframe src="https://maps.google.com/maps?q=Bamboomedia&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0"
+            style="border:0" allowfullscreen></iframe>
+        </div>
+        <br>
+
+        <!--Deskripsi-->
+        <div class="row text-center">
+          <div class="col-md-4">
+            <a class="btn-floating blue accent-1"><i class="fas fa-map-marker-alt"></i></a>
+            <p>Jalan Merdeka No. 45</p>
+            <p>Kota Denpasar, Bali</p>
+          </div>
+
+          <div class="col-md-4">
+            <a class="btn-floating blue accent-1"><i class="fas fa-phone"></i></a>
+            <p>+ 01 234 567 89</p>
+            <p>Mon - Sat, 8:00-18:00</p>
+          </div>
+
+          <div class="col-md-4">
+            <a class="btn-floating blue accent-1"><i class="fas fa-envelope"></i></a>
+            <p>pryandika7@gmail.com</p>
+            <p>ngrmertha@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
-</section>
 </template>
 
 <script>
@@ -13,6 +44,21 @@
 
 <style>
 .contact {
-  padding:50px;
+  padding:60px;
 }
+
+  .map-container-6{
+  overflow:hidden;
+  padding-bottom:56.25%;
+  position:relative;
+  height:0;
+  }
+
+  .map-container-6 iframe{
+  left:0;
+  top:0;
+  height:100%;
+  width:100%;
+  position:absolute;
+  }
 </style>
