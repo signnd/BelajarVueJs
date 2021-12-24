cf1269ed4494c0b0d607f7292dc4d48eebfa1e9c
signnd <nfffqdb@gmail.com>
2021-12-24 14:47:38 +0800

    Fix typo on about & header on doctors

--- src/views/Doctors.vue
+++ src/views/Doctors.vue
@@ -1,18 +1,48 @@
 <template>
 <section>
-<div class="doctors" id="doctors">
-    <h1 class="mt-5">Dokter</h1>
-    <h5 class="mt-2">Nol koma wiro sableng</h5>
-    <p>Kami belum punya dokter 🙏</p>
-  </div>
+    <div class="doctors" id="doctors">
+      <div class="container-fluid p-0">
+        <img :src="images.cover1" alt="habout" class="w-100">
+        <h1 class="mt-5 centered text-black style fw-bold" style="font-size:3vw;">Our doctors</h1>
+      </div>
+    </div>
 </section>
+  <!-- Content -->
+  <section>
+      <h5 align="middle" class="my-5">Coming soon</h5>
+  </section>
 </template>
 
 <script>
+export default {
+    data() {
+        return {
+            images: {
+                cover1: require('@/assets/hero/flat.png'),
+                cover2: require('@/assets/gallery/section_bg01.png')
+            }
+        }
+    }
+}
 </script>
 
 <style>
-.doctors {
-  padding:50px;
+.container{
+  position: relative;
+}
+
+.container-fluid {
+  overflow: hidden;
+  position: relative;
+  text-align: left;
+  width: 100%;
+
+}
+
+.centered{  
+  position: absolute;
+  top: 42%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
