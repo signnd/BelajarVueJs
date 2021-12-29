1e4f202a1aaba702330271b55d058e6e41d4252f
signnd <nfffqdb@gmail.com>
2021-12-29 16:47:52 +0800

    commit?

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,6 @@
 <template>
+  <section>
   <div class="contact">
-    <section>
       <!--Section heading-->
       <div class="contacthead" id="contacthead">
         <div class="container-fluid px-0">
@@ -12,8 +12,8 @@
           <p class="d-block d-lg-none text-center my-2">Anda dapat menghubungi kami melalui:</p>
         </div>
       </div>
+      </div>
     </section>
-  </div>
   
   <section>
     <!-- Whole Card -->
@@ -128,7 +128,23 @@ export default {
   position: relative;
   text-align: left;
   width: 100%;
-
+}
+.iframe-container{
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%;
+}
+.iframe-container > *{
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
 }
 
 .centered{  
