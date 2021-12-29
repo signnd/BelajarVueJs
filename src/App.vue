5624a69093db136625e5751e1feb1bf8f1374d27
signnd <nfffqdb@gmail.com>
2021-12-29 09:28:29 +0800

    tambah stylesheet icon

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
   <!-- Navbar atas -->
   <section>
   <div class="header-area" id="nav">
