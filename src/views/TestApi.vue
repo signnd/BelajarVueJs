09a37067e95032cb3b8bf04f2c99c52b722146ca
Pasek Ryandika <91819749+Pryandika@users.noreply.github.com>
2021-12-30 10:45:54 +0800

    added axios, routing and /api

--- src/views/TestApi.vue
+++ src/views/TestApi.vue
@@ -1,32 +1,8 @@
 <template>
-<div id="app">
-  <h1 class="pt-5 mt-5"> Price Index</h1>
-  <div v-for="currency in info2" v-bind:key="currency" class="currency">
-    {{ currency.description }}:
-    <span class="lighten">
-      <span v-html="currency.symbol"></span>{{ currency.rate_float }}
-    </span>
-  </div>
-   <div>
-   </div>
-</div>
 </template>
 
 <script>
-import axios from "axios";
-
-export default {
-  data: () => ({
-    info: null,
-    info2: null
-  }),
-  created() {
-    axios
-  .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-  .then(response => (this.info2 = response.data.bpi))
-  }
-};
 </script>
 
-<style scoped lang="scss">
+<style>
 </style>
