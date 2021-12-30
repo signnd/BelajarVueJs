6ff83244859f4ee224dbf6e328fa1820cefbfbcb
Pasek Ryandika <91819749+Pryandika@users.noreply.github.com>
2021-12-30 15:09:17 +0800

    added more on api

--- src/views/TestApi.vue
+++ src/views/TestApi.vue
@@ -2,6 +2,7 @@
 <div id="app">
   <h1 class="pt-5 mt-5"></h1>
   {{at}}
+  {{ab}}
 </div>
 </template>
 
@@ -16,6 +17,7 @@ export default {
 axios.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d7667d2d21805a0a483e3da7ce1a0c9b')
   .then((response) => {
     this.at = response.data.weather[0].main;
+    this.ab = response.data;
   });
   }
 };
