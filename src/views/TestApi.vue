75050d9268fd82db9da6caf2128dabcbb2408ecc
signnd <nfffqdb@gmail.com>
2021-12-30 14:45:17 +0800

    Revert "Success building a scuffed GET API"
    This reverts commit 0d4631440585d6a4e5161e0ac7d16692d3b712b1.

--- src/views/TestApi.vue
+++ src/views/TestApi.vue
@@ -1,24 +1,7 @@
 <template>
-<div id="app">
-  <h1 class="pt-5 mt-5"></h1>
-  {{at}}
-</div>
 </template>
 
 <script>
-import axios from "axios";
-export default {
-   data: () => ({
-    at: null,
-    ab: null
-  }),
-  created() {
-axios.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d7667d2d21805a0a483e3da7ce1a0c9b')
-  .then((response) => {
-    this.at = response.data.weather[0].main;
-  });
-  }
-};
 </script>
 
 <style>
