2c184ba6291940f56922a659ffde0f16d6ad2b50
Pasek Ryandika <91819749+Pryandika@users.noreply.github.com>
2022-01-04 16:48:09 +0800

    Menambahkan modal di members

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -20,7 +20,6 @@
           <option hidden>Lokasi</option>
           <option>Denpasar</option>
           <option>Tabanan</option>
-          <option>Lokasi</option>
           <option>Klungkung</option>
           <option>Gianyar</option>
           <option>Buleleng</option>
@@ -60,29 +59,36 @@
           <div class="card-body text-center">
             <h5 class="card-title">Rumah Sakit 1</h5>
             <p class="card-text">Deskripsi</p>
-            <router-link to="/About" class="btn d-block btn-outline-primary text-wrap">Reservasi</router-link>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+              Reservasi
+            </button>
           </div>
         </div>
         <div class="card" style="width:auto;">
-          <img class="card-img-top" :src="images.placeholder" alt="Card image 2">
+          <img class="card-img-top" :src="images.placeholder" alt="Card image 1">
           <div class="card-body text-center">
             <h5 class="card-title">Rumah Sakit 2</h5>
             <p class="card-text">Deskripsi</p>
-            <router-link to="/Contact" class="btn d-block btn-outline-primary text-wrap">Reservasi</router-link>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+              Reservasi
+            </button>
           </div>
         </div>
         <div class="card" style="width:auto;">
-          <img class="card-img-top" :src="images.placeholder" alt="Card image 2">
+          <img class="card-img-top" :src="images.placeholder" alt="Card image 1">
           <div class="card-body text-center">
             <h5 class="card-title">Rumah Sakit 3</h5>
             <p class="card-text">Deskripsi</p>
-            <router-link to="/Members" class="btn d-block btn-outline-primary text-wrap">Reservasi</router-link>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+              Reservasi
+            </button>
           </div>
         </div>
       </div>
     </div>
   </div>
 </section>
+
 <section>
   <div class="container d-flex">
     <div class="container my-5 align-items-center" style="width: 900px;">
@@ -92,29 +98,36 @@
           <div class="card-body text-center">
             <h5 class="card-title">Rumah Sakit 4</h5>
             <p class="card-text">Deskripsi</p>
-            <router-link to="/About" class="btn d-block btn-outline-primary text-wrap">Reservasi</router-link>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+              Reservasi
+            </button>
           </div>
         </div>
         <div class="card" style="width:auto;">
-          <img class="card-img-top" :src="images.placeholder" alt="Card image 2">
+          <img class="card-img-top" :src="images.placeholder" alt="Card image 1">
           <div class="card-body text-center">
             <h5 class="card-title">Rumah Sakit 5</h5>
             <p class="card-text">Deskripsi</p>
-            <router-link to="/Contact" class="btn d-block btn-outline-primary text-wrap">Reservasi</router-link>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+              Reservasi
+            </button>
           </div>
         </div>
         <div class="card" style="width:auto;">
-          <img class="card-img-top" :src="images.placeholder" alt="Card image 2">
+          <img class="card-img-top" :src="images.placeholder" alt="Card image 1">
           <div class="card-body text-center">
             <h5 class="card-title">Rumah Sakit 6</h5>
             <p class="card-text">Deskripsi</p>
-            <router-link to="/Members" class="btn d-block btn-outline-primary text-wrap">Reservasi</router-link>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+              Reservasi
+            </button>
           </div>
         </div>
       </div>
     </div>
   </div>
 </section>
+
 <section>
   <div class="container d-flex">
     <div class="container my-5 align-items-center" style="width: 900px;">
@@ -124,23 +137,29 @@
           <div class="card-body text-center">
             <h5 class="card-title">Rumah Sakit 7</h5>
             <p class="card-text">Deskripsi</p>
-            <router-link to="/About" class="btn d-block btn-outline-primary text-wrap">Reservasi</router-link>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+              Reservasi
+            </button>
           </div>
         </div>
         <div class="card" style="width:auto;">
-          <img class="card-img-top" :src="images.placeholder" alt="Card image 2">
+          <img class="card-img-top" :src="images.placeholder" alt="Card image 1">
           <div class="card-body text-center">
             <h5 class="card-title">Rumah Sakit 8</h5>
             <p class="card-text">Deskripsi</p>
-            <router-link to="/Contact" class="btn d-block btn-outline-primary text-wrap">Reservasi</router-link>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+              Reservasi
+            </button>
           </div>
         </div>
         <div class="card" style="width:auto;">
-          <img class="card-img-top" :src="images.placeholder" alt="Card image 2">
+          <img class="card-img-top" :src="images.placeholder" alt="Card image 1">
           <div class="card-body text-center">
             <h5 class="card-title">Rumah Sakit 9</h5>
             <p class="card-text">Deskripsi</p>
-            <router-link to="/Members" class="btn d-block btn-outline-primary text-wrap">Reservasi</router-link>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+              Reservasi
+            </button>
           </div>
         </div>
       </div>
@@ -148,7 +167,50 @@
   </div>
 </section>
 
-
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Reservasi</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <img :src="images.speedid" alt="speedid" class="speedid-size pt-3">
+        <div class="modal-body">
+          <h4 class="font-weight-bold">
+            Anda dapat melakukan registrasi langsung lewat aplikasi SpeedID dengan mencari rumah sakit "Rumah Sakit Ini" pada fitur SpeedQ.
+          </h4>
+          <br>
+          <h5 class="text-left">Belum Install?</h5>
+          <ol class="text-left">
+            <li>Download SpeedID</li>
+            <li>Klik SpeedQ</li>
+            <li>Cari Lokasi yang Anda Inginkan</li>
+            <li>Tambahkan "Rumah Sakit Ini" ke Favorite (Klik ★)</li>
+            <li>Pilih Dokter dan Hari Layanan</li>
+            <li>Tiket Anda di My-Ticket</li>
+          </ol>
+        </div>
+        <div class="modal-footer">
+          <div class="containe">
+            <div class="row">
+              <div class="col">
+                <a href="https://play.google.com/store/apps/details?id=com.bamboomedia.speedid&hl=in&gl=US"
+                  target="_blank"><img :src="images.playstore" alt="playstore" class="logo-download"></a>
+              </div>
+              <div class="col">
+                <a href="https://apps.apple.com/id/app/speedid/id1439413446" target="_blank"><img :src="images.apple"
+                    alt="apple" class="logo-download"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   
 
@@ -162,7 +224,10 @@ export default {
             images: {
                 cover1: require('@/assets/hero/flat.png'),
                 cover2: require('@/assets/gallery/section_bg01.png'),
-                placeholder: require('@/assets/placeholder.png')                
+                placeholder: require('@/assets/placeholder.png'),    
+                apple: require('@/assets/modal/apple.png'), 
+                playstore: require('@/assets/modal/playstore.png'), 
+                speedid: require('@/assets/modal/SpeedID.png')            
             }
         }
     }
@@ -187,5 +252,15 @@ export default {
   top: 42%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+
+.speedid-size{
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+}
+
+.logo-download{
+ width: 100%;
 }
 </style>
