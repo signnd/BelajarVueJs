dce5252ae5b59d082a648f3bef6fd75881c7c8ec
signnd <nfffqdb@gmail.com>
2022-01-05 12:07:20 +0800

    tambah halaman di members + edit-edit nama RS

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,9 @@ import About from '../views/About.vue'
 import Contact from '../views/Contact.vue'
 import Members from '../views/Members.vue'
 import TestApi from '../views/TestApi.vue'
+import RSUPSanglah from '../views/Members/rsup-sanglah.vue'
+import RSUBM from '../views/Members/rsu-balimandara.vue'
+import RSMBM from '../views/Members/rsm-balimandara.vue'
 const routes = 
 [
   {
@@ -33,7 +36,22 @@ const routes =
       path: '/api',
       name: 'api',
       component: TestApi
-    }
+    },
+  {
+    path: '/members/rsup-sanglah',
+    name: 'members/rsup-sanglah',
+    component: RSUPSanglah
+  },
+  {
+    path: '/members/rsu-balimandara',
+    name: 'members/rsu-balimandara',
+    component: RSUBM
+  },
+  {
+    path: '/members/rsm-balimandara',
+    name: 'members/rsm-balimandara',
+    component: RSMBM
+  }
 ]
 
 const router = createRouter({
