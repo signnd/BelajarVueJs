4777fbc10340c01311fcce03999d190e49562e58
Pasek Ryandika <91819749+Pryandika@users.noreply.github.com>
2022-01-06 16:26:36 +0800

    almost complete version of popular destination

--- src/App.vue
+++ src/App.vue
@@ -63,10 +63,12 @@
 
   <section>
     <div class="footer">
-      <div class="row justify-content-center">
-      <div class="col-1"><a class="text-dark" href="https://facebook.com"><i class="bi bi-facebook"></i></a></div>
-      <div class="col-1"><a class="text-dark" href="https://whatsapp.com"><i class="bi bi-whatsapp"></i></a></div>
-      <div class="col-1"><a class="text-dark" href="https://instagram.com"><i class="bi bi-instagram"></i></a></div>
+      <div class="container-fluid">
+        <div class=" row justify-content-center">
+          <div class="col-1"><a class="text-dark" href="https://facebook.com"><i class="bi bi-facebook"></i></a></div>
+          <div class="col-1"><a class="text-dark" href="https://whatsapp.com"><i class="bi bi-whatsapp"></i></a></div>
+          <div class="col-1"><a class="text-dark" href="https://instagram.com"><i class="bi bi-instagram"></i></a></div>
+        </div>
       </div>
       <div class="py-2"></div>
       <div class="d-block d-sm-none">© 2021 - {{new Date().getFullYear()}} Medical Tourism <br><br>
