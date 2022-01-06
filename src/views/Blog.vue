f2c80c5e5b2951cb46ba00688c059ab8d7548940
CEL12345 <michaelmarcel0911@gmail.com>
2022-01-06 10:24:57 +0800

    update blog

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -9,7 +9,7 @@
     
     
    <div class="container-fluid d-flex">
-    <div class="container-fluid ml-5 my-5 pt-5 mt-5 align-items-center" style="width: 1500px;">
+    <div class="container-fluid ml-5 my-5 pt-5 mt-5 align-items-center" style="width: 1000px;">
       <h1 class="">Artikel Kami</h1>
         <div class="card-group">
           <div class="card" style="width:auto;">
@@ -18,38 +18,61 @@
                 <h5 class="card-title">Artikel Belum Tersedia</h5>
                 <p class="card-text">Kosong</p>
                   <router-link to="/About" class="btn d-block btn-outline-primary text-wrap">Lebih Lanjut</router-link>
+                <div class="row">
+                <div class="col">
+                 Info Kesehatan
+               </div>
                 </div>
               </div>
-               
-          <div class="card" style="width:auto;">
-            <img class="card-img-top" :src="images.card1" alt="Card image 2">
-              <div class="card-body">
-                <h5 class="card-title">Artikel Belum Tersedia</h5>
-                <p class="card-text">Kosong</p>
-                  <router-link to="/About" class="btn d-block btn-outline-primary text-wrap">Lebih Lanjut</router-link>
 
-                   <div class="card" style="width:auto;">
-            <img class="card-img-top" :src="images.card1" alt="Card image 2">
+
+
+        <div class="card" style="width:auto;">
+          <img class="card-img-top" :src="images.card1" alt="Card image 2">
+            <div class="card-body">
+             <h5 class="card-title">Artikel Belum Tersedia</h5>
+              <p class="card-text">Kosong</p>
+                <router-link to="/About" class="btn d-block btn-outline-primary text-wrap">Lebih Lanjut</router-link>
+                  <div class="col">
+                   Info Rumah Sakit
+                   </div>
+
+
+
+          <div class="card" style="width:auto;">
+            <img class="card-img-top" :src="images.card1" alt="Card image 3">
               <div class="card-body">
                 <h5 class="card-title">Artikel Belum Tersedia</h5>
-                <p class="card-text">Kosong</p>
-                  <router-link to="/About" class="btn d-block btn-outline-primary text-wrap">Lebih Lanjut</router-link>
-               
-              <a class="carousel-control-prev" href="#slideshow" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#slideshow" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-                 </div>
+                  <p class="card-text">Kosong</p>
+                <router-link to="/About" class="btn d-block btn-outline-primary text-wrap">Lebih Lanjut</router-link>
+             <div class="col">
+           Info 
+          </div>
+
+          <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center">
+             <li class="page-item disabled">
+               <a class="page-link" href="#" tabindex="-1">Previous</a>
+                 </li>
+                   <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+             <li class="page-item">
+           <a class="page-link" href="#">Next</a>
+          </li>
+         </ul>
+        </nav>
+          </div>
+           </div>
+            </div>
+             </div>
+               </div>
+                </div>
                </div>
               </div>
-             </div>
-           </div>
-           </div>
-           </div>
+          
+           
+
   </section>
 
   </template>
