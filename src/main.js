99596d44e00b70e57ba1982124a22b3996d23f2d
Pasek Ryandika <91819749+Pryandika@users.noreply.github.com>
2022-01-19 15:25:27 +0800

    mulai API stuff

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 const app = createApp(App)
 app.config.globalProperties.$translate = (arr) => {
     let language = localStorage.getItem('language')
