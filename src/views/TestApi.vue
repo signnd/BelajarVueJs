48169bc0a691c181a6af91915134ff6522d8cc35
Pasek Ryandika <91819749+Pryandika@users.noreply.github.com>
2022-01-20 11:22:54 +0800

    real api testing

--- src/views/TestApi.vue
+++ src/views/TestApi.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="api" class="mt-5 pt-5">
-    {{data}}
+    <!-- {{data}} -->
   </div>     
 </template>
 
@@ -9,14 +9,18 @@ import axios from "axios";
 
 export default {
   data: () => ({
-    data: null
+    items: null,
+    weather: null
   }),
     created(){
-      axios.get('https://api.openweathermap.org/data/2.5/weather?id=7038902&units=metric&APPID=d7667d2d21805a0a483e3da7ce1a0c9b')
+      let baseUrl = 'https://kimiafarmadenpasar.co.id/api_bmta';
+      // axios.get(`${'https://cors-anywhere.herokuapp.com/'}${baseUrl}/grouped_counters.php`)
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?id=7038902&units=metric&APPID=d7667d2d21805a0a483e3da7ce1a0c9b`)
       .then((response) => {
-        this.data = response.data;
+        this.data = response.data.weather;
         console.log(this.data);
-      });
+      })
+      //  .catch(error => console.log('Request failed'));
     }
   }
 </script>
