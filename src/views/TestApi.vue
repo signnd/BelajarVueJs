1528e656211658950ab65b95393da31f80d84798
Pasek Ryandika <91819749+Pryandika@users.noreply.github.com>
2022-01-21 16:58:16 +0800

    TES API di slash API

--- src/views/TestApi.vue
+++ src/views/TestApi.vue
@@ -1,34 +1,83 @@
 <template>
-  <div id="api" class="mt-5 pt-5">
-    <form class="d-none d-md-block">
-    <div class="container-fluid form-row f-color px-5">
-      <div class="col-3 ml-5">
-        <select class="form-control form-control-md">
-          <option hidden>{{$translate(['Lokasi','Location'])}}</option>
-          <option>Denpasar</option>
-          <option>Tabanan</option>
-          <option>Klungkung</option>
-          <option>Gianyar</option>
-          <option>Buleleng</option>
-        </select>
-      </div>
-      <div class="col-3">
-        <select class="form-control form-control-md">
-          <option hidden>{{$translate(['Kategori','Categories'])}}</option>
-          <option v-for="kategori in this.poli" :key="kategori.id">{{kategori.name}}</option>
-        </select>
-      </div>
-      <div class="col-3">
-        <input class="form-control mr-2" type="search" placeholder="Search">
-      </div>
-      <div class="col-2">
-        <router-link :to="{name: 'Destination'}">
-          <button type="button" class="btn btn-md btn-block btn-primary">{{$translate(['Cari','Search'])}}</button>
-        </router-link>
+<div class="container d-flex">
+    <div class="container my-5 align-items-center" style="width: 900px;">
+      <div class="card-group">
+        <div class="card" style="width:auto;">
+          <img class="card-img-top" src="" id="img1" alt="RSUP Sanglah">
+          <div class="card-body text-center">
+            <h5 class="card-title" id="rs1"></h5>
+            <p class="card-text">Jl. Diponegoro, Denpasar</p>
+            <div class="pt-4 d-none d-sm-block"></div>
+            <router-link to="" class="btn d-block btn-outline-primary text-wrap gap-2">Detail
+            </router-link>
+            <div class="py-1"></div>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col">
+                  <button class="btn w-100 btn-primary" data-toggle="modal"
+                    data-target="#Reservasi">{{$translate(['Reservasi','Reservation'])}}</button>
+                </div>
+                <div class="py-1 d-block d-sm-block d-md-block d-lg-none"></div>
+                <div class="col">
+                  <button class="btn w-100 btn-primary" data-toggle="modal"
+                    data-target="#Jadwal">{{$translate(['Jadwal','Schedule'])}}</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card" style="width:auto;">
+          <img class="card-img-top" src="" alt="RSU Bali Mandara">
+          <div class="card-body text-center">
+            <h5 class="rs2 card-title" id="rs2">RSU Bali Mandara</h5>
+            <p class="card-text">Jl. Bypass Ngurah Rai No. 548, Denpasar</p>
+            <router-link to="" class="btn d-block btn-outline-primary text-wrap">Detail
+            </router-link>
+            <div class="py-1"></div>
+            <div class="container-fluid">
+            <div class="row">
+              <div class="col">
+                <button class="btn w-100 btn-primary" data-toggle="modal"
+                  data-target="#Reservasi">{{$translate(['Reservasi','Reservation'])}}</button>
+              </div>
+                <div class="py-1 d-block d-sm-block d-md-block d-lg-none"></div>
+              <div class="col">
+                <button class="btn w-100 btn-primary" data-toggle="modal"
+                  data-target="#Jadwal">{{$translate(['Jadwal','Schedule'])}}</button>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="card" style="width:auto;">
+          <img class="card-img-top" src="" alt="RS Mata Bali Mandara">
+          <div class="card-body text-center">
+            <h5 class="rs card-title" id="rs3">RS Mata Bali Mandara</h5>
+            <p class="card-text">Jl. Angsoka No. 8 Denpasar</p>
+            <div class="pt-4 d-none d-sm-block d-md-none d-lg-block"></div>
+            <router-link to="" class="btn d-block btn-outline-primary text-wrap">Detail
+            </router-link>
+            <div class="pt-2"></div>
+            <div class="row">
+              <div class="col">
+                <button class="btn w-100 btn-primary" data-toggle="modal"
+                  data-target="#Reservasi">{{$translate(['Reservasi','Reservation'])}}</button>
+              </div>
+                <div class="py-1 d-block d-sm-block d-md-block d-lg-none"></div>
+              <div class="col">
+                <button class="btn w-100 btn-primary" data-toggle="modal"
+                  data-target="#Jadwal">{{$translate(['Jadwal','Schedule'])}}</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-</form>
-  </div>     
+  </div>
+
+  <ul>
+    <li v-for="klinik in poli" :key="klinik.id">{{this.poli}}</li>
+  </ul>
 </template>
 
 <script>
@@ -37,23 +86,23 @@ import axios from "axios";
 export default {
   data: () => ({
     poli: null,
-    jadwal: null
+    jadwal: null,
+    nama: null,
+    deskripsi: null,
+    img: null,
   }),
     created(){
       let baseUrl = 'https://kimiafarmadenpasar.co.id/api_bmta';
-      axios.get(`${'https://cors-anywhere.herokuapp.com/'}${baseUrl}/grouped_counters.php`)
-      // axios.get(`https://api.openweathermap.org/data/2.5/weather?id=7038902&units=metric&APPID=d7667d2d21805a0a483e3da7ce1a0c9b`)
+      axios.get(`${'https://cors-anywhere.herokuapp.com/'}${baseUrl}/offices.php?page=1&lat=-8.6649188&long=115.2384802`)
       .then((response) => {
-        this.poli = response.data.data.items;
-        console.log(this.poli);
+        this.img = response.data.data.items[0].images[1];
+        console.log(this.img);
+        document.getElementById("img1").src = this.img;
       })
-
-      // axios.get(`${'https://cors-anywhere.herokuapp.com/'}${baseUrl}/operational_days.php?office_id=1&counter_id=1`)
-      // .then((response) => {
-      //   this.jadwal = response.data.data.items;
-      //   console.log(this.jadwal);
-      // })
-      //  .catch(error => console.log('Request failed'));
+      axios.get(`${'https://cors-anywhere.herokuapp.com/'}${baseUrl}/grouped_counters.php`)
+      .then((response) => {
+        this.poli = response.data.data.items[0];
+      })
     }
   }
 </script>
